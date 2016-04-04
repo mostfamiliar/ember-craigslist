fb2337f0f4c12a54635b61543ce21aee11b6277d
torrence stratton <tlstratton@gmail.com>
2016-04-04 11:57:40 -0700

    add styling to page

--- app/models/listing.js
+++ app/models/listing.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   title: DS.attr(),
   location: DS.attr(),
   date: DS.attr(),
+  email: DS.attr(),
   description: DS.attr(),
   image: DS.attr(),
   category: DS.belongsTo('category', { async: true })

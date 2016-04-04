df4192f0a4e3028751331ac72abb574113403106
torrence stratton <tlstratton@gmail.com>
2016-04-04 10:17:55 -0700

    add working sort by date

--- app/components/single-category.js
+++ app/components/single-category.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['date:asc'],
+  sortedByDate: Ember.computed.sort('category.listings', 'sortBy')
+  sortByTitle: ['title:asc'];
+  sortedByTitle: Ember.computed.sort('category.listings', 'sortByTitle');
 });

ebc89b5e356103f3366f07f3b74703472331dc32
torrence stratton <tlstratton@gmail.com>
2016-04-04 10:47:09 -0700

    get two sort options working

--- app/components/single-category.js
+++ app/components/single-category.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortBy: ['date:asc'],
-  sortedByDate: Ember.computed.sort('category.listings', 'sortBy')
-  sortByTitle: ['title:asc'];
-  sortedByTitle: Ember.computed.sort('category.listings', 'sortByTitle');
+  sortedListings: Ember.computed.sort('category.listings', 'sortDefinition'),
+  sortBy: 'date',
+  sortDefinition: Ember.computed('sortBy', function(){
+    return [ this.get('sortBy') ];
+  })
 });
